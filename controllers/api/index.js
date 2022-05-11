e73cf3bcb08612cee7417bef7f0881184c40919d
John-Pena <jmp1495@gmail.com>
2022-05-10 20:31:14 -0500

    created route for post to receive all post data

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 // User, Post, and Comment routes go here
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
 // router.use for User, Post, and Comment routes go here
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,1 +1,39 @@
-// TODO
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment, Like } = require('../../models');
+
+// Get all 
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id', 
+      'title', 
+      'content'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(postData => res.json(postData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// Get by id
+
+
+// create a post
+
+
+// update post by id
+
+
+// delete post
+
+
+module.exports = router;
