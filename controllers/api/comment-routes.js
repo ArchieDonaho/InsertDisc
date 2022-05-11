f656e3f0c6d514d14793bb60ba03907bb9b47940
John-Pena <jmp1495@gmail.com>
2022-05-10 21:04:42 -0500

    created api route for comments. only have route to recieve all comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,1 +1,20 @@
-// TODO
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// Get all comments
+router.get('/', (req, res) => {
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Creates a comment
+
+
+// Delete a comment
+
+
+module.exports = router;
