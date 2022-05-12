cf89b516a2ed419fac27a514e004a70e5aa22e72
AD <archied5150@yahoo.com>
2022-05-11 20:41:53 -0500

    added dashboard routes and user authenitcation

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Post, Comment } = require('../models');
+const { Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // render the dashboard with all the user's posts
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
   Post.findAll({
     where: {
       // use the ID from the session
