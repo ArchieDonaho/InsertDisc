0a3676762ebb94d58195702f40ec0a87290cc660
John-Pena <jmp1495@gmail.com>
2022-05-11 19:57:30 -0500

    added catagory column to post-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,9 @@
-// TODO
+const router = require('express').Router();
+const { Post, Like, User, Comment } = require('../models');
+
+// Get all routes for homepage
+router.get('/', (req, res) => {
+
+})
+
+module.exports = router;
